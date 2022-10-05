5259ddbfad425985b7c39f329ed04e4fb893db29
cos7els <cos7els@gmail.com>
2022-10-05 15:14:36 +0300

    Fix some bugs

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        try (Scanner in = new Scanner(System.in)) {
-            System.out.print("Enter the login: ");
-            String login = in.nextLine();
-            System.out.print("Enter the password: ");
-            String password = in.nextLine();
-            System.out.print("Confirm the password: ");
-            String confirmPassword = in.nextLine();
-            Registrator.register(login, password, confirmPassword);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(e.getMessage());
-        }
+        Scanner in = new Scanner(System.in);
+        do {
+            try {
+                System.out.print("Enter the login: ");
+                String login = in.nextLine();
+                System.out.print("Enter the password: ");
+                String password = in.nextLine();
+                System.out.print("Confirm the password: ");
+                String confirmPassword = in.nextLine();
+                Registrator.register(login, password, confirmPassword);
+            } catch (WrongLoginException | WrongPasswordException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.print("Do you want exit? y/n: ");
+        } while (!in.nextLine().equalsIgnoreCase("y"));
+        in.close();
     }
+
 }
--- src/Task2/Registrator.java
+++ src/Task2/Registrator.java
@@ -2,7 +2,8 @@ package Task2;
 
 public class Registrator {
 
-    public static boolean register(String login, String password, String confirmPassword) throws Exception {
+    public static boolean register(String login, String password, String confirmPassword) throws WrongLoginException,
+            WrongPasswordException {
         boolean result = false;
         result = login(login);
         result = password(password, confirmPassword);
@@ -16,7 +17,7 @@ public class Registrator {
         } else if (login.length() >= 20) {
             throw new WrongLoginException("Wrong login, your login is too long");
         } else if (login.contains(" ")) {
-            throw new WrongLoginException("Wrong login, your login contains ' '");
+            throw new WrongLoginException("Wrong login, your login contains space symbol");
         } else {
             System.out.println("Login is OK");
             result = true;
@@ -28,19 +29,19 @@ public class Registrator {
         boolean result = false;
         if (!password.equals(confirmPassword)) {
             throw new WrongPasswordException("Wrong password, password and confirm password is not equals");
-        } else if (password.length() >= 20 && password.contains(" ") & !password.replaceAll("[a-zA-Z]", "").matches("[0-9]+")) {
+        } else if (password.length() >= 20 & password.contains(" ") & !password.replaceAll("\\D+", "").matches("\\d+")) {
             throw new WrongPasswordException("Wrong password, password is too long, contains space symbols and not contains number");
-        } else if (password.length() >= 20 && password.contains(" ")) {
+        } else if (password.length() >= 20 & password.contains(" ")) {
             throw new WrongPasswordException("Wrong password, password is too long and contains space symbols");
-        } else if (password.length() >= 20 && !password.replaceAll("[a-zA-Z]", "").matches("[0-9]+")) {
+        } else if (password.length() >= 20 & !password.replaceAll("\\D+", "").matches("\\d+")) {
             throw new WrongPasswordException("Wrong password, password is too long and not contains number");
-        } else if (password.contains(" ") && !password.replaceAll("[a-zA-Z]", "").matches("[0-9]+")) {
+        } else if (password.contains(" ") & !password.replaceAll("\\D+", "").matches("\\d+")) {
             throw new WrongPasswordException("Wrong password, password contains space symbols and not contains number");
         } else if (password.length() >= 20) {
             throw new WrongPasswordException("Wrong password, password is too long");
         } else if (password.contains(" ")) {
             throw new WrongPasswordException("Wrong password, password contains space symbols");
-        } else if (!password.replaceAll("[a-zA-Z]", "").matches("[0-9]+")) {
+        } else if (!password.replaceAll("\\D+", "").matches("\\d+")) {
             throw new WrongPasswordException("Wrong password, password is not contains number");
         } else {
             System.out.println("Password is OK");

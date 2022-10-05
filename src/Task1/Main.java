e6b951f32afe7c11863c3b410c2fb116eff256e5
cos7els <cos7els@gmail.com>
2022-10-05 15:20:14 +0300

    Fix some bugs

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,19 +1,24 @@
 package Task1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        try {
-//            WorkWithStrings.isContainsABC("1234-abc-5678-def-9g0h");
-//            WorkWithStrings.isContainsABC("1234-cba-5678-def-9g0h");
-//            WorkWithStrings.isStartFrom555("1234-abc-5678-def-9g0h");
-//            WorkWithStrings.isStartFrom555("5555-abc-5678-def-9g0h");
-//            WorkWithStrings.isEndWith1a2b("5555-abc-5678-def-9g0h");
-//            WorkWithStrings.isEndWith1a2b("5555-abc-5678-def-1a2b");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(e.getMessage());
-        }
+        Scanner in = new Scanner(System.in);
+        do {
+            try {
+                System.out.print("Enter the string: ");
+                String string = in.nextLine();
+                WorkWithStrings.isContainsABC(string);
+                WorkWithStrings.isStartFrom555(string);
+                WorkWithStrings.isEndWith1a2b(string);
+            } catch (ContainsABCException | EndWith1A2BException | StartFrom555Exception e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.print("Do you want exit? y/n: ");
+        } while (!in.nextLine().equalsIgnoreCase("y"));
+        in.close();
     }
 
 }
